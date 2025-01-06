7664048f671ea26289f0a848cf2bc00690f4c46b
김정희 <jenny8530@naver.com>
2025-01-07 01:26:53 +0900

    feat: implement settlement status screen

---
+++ app/src/main/java/com/example/madcampweek2/calculate/CalculateHostActivity.kt
@@ -0,0 +1,21 @@
+package com.example.madcampweek2.calculate
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.madcampweek2.R
+
+class CalculateHostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_calculate_host)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
