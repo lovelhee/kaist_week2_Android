fb97bc9d19ba71a5ccc89fb5916cab11f831111d
김정희 <jenny8530@naver.com>
2025-01-07 16:49:21 +0900

    feat: add input text field for entering friends

--- app/src/main/java/com/example/madcampweek2/makeRoom/HostActivity.kt
+++ app/src/main/java/com/example/madcampweek2/makeRoom/HostActivity.kt
@@ -2,7 +2,13 @@ package com.example.madcampweek2.makeRoom
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,6 +26,34 @@ class HostActivity : AppCompatActivity() {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        val etInvite: EditText = findViewById(R.id.etInvite)
+        val btnInvite: Button = findViewById(R.id.btnInvite)
+        val invitedFriendsLayout: LinearLayout = findViewById(R.id.invitedFriendsLayout)
+
+        btnInvite.setOnClickListener {
+            val friendName = etInvite.text.toString()
+
+            if (friendName.isNotBlank()) {
+
+                val textView = TextView(this).apply {
+                    text = friendName
+                    textSize = 16f
+                    setTextColor(resources.getColor(android.R.color.black, null))
+                    layoutParams = LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                    ).apply {
+                        setMargins(0, 10, 0, 10)
+                    }
+                    gravity = Gravity.START
+                }
+
+                invitedFriendsLayout.addView(textView)
+
+                etInvite.text.clear()
+            }
         }
     }
 }
